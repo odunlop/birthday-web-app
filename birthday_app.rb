473e65abc6b2b5ed20a9fe294dc642f1e9e3bf7c
Orla <orladunlop2@gmail.com>
2022-01-13 12:30:45 +0000

    Introducing CSS stylesheet

--- birthday_app.rb
+++ birthday_app.rb
@@ -6,15 +6,19 @@ require "./lib/birthday_checker"
 class DayCount < Sinatra::Base
 
   get '/' do
-    erb(:index)
+    erb(:pretty_index)
   end
   post '/birthday' do 
     @name = params[:name]
-    birthday = BirthdayChecker.new(params[:day], params[:month])
-    @days_left = birthday.days_left
+    @birthday = BirthdayChecker.new(params[:day], params[:month])
+    @days_left = @birthday.days_left
     
     erb(:countdown)
   end
-  
+
+  get '/test' do
+    erb(:pretty_index)
+  end
+
   run! if app_file == $0
 end
--- spec/feature/homepage_spec.rb
+++ spec/feature/homepage_spec.rb
@@ -1,12 +1,31 @@
 describe "homepage", type: :feature do
   it "a user sees how many days until their birthday" do
-    
+
     visit '/' 
     fill_in "Name", with: "Orla"
     fill_in :day, with: "6"
     select "February", :from => "month"
     click_button "Submit"
 
-    expect(page).to have_content "Orla - there are 25 days left until your birthday"
+    expect(page).to have_content "Orla - there are 24 days left until your birthday"
+  end
+
+  it "a user sees a different message if it's their birthday today" do
+
+    visit '/'
+    fill_in "Name", with: "Alex"
+    fill_in :day, with: "#{Time.now.day}"
+    select "#{month_convertor(Time.now.month)}", :from => "month"
+    click_button "Submit"
+
+    expect(page).to have_content "Congrats Alex! It's your birthday today!"
+  end
+
+  def month_convertor(int)
+    months = { 1 => "January", 2 => "February", 3 => "March",
+               4 => "April", 5 => "May", 6 => "June", 7 => "July",
+               8 => "August", 9 => "September", 10 => "October",
+               11 => "November", 12 => "December"}
+    months[int]
   end
 end
